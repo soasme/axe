f7d9f0289d0d26de5b5e7a9e8894d5f62e9042a6
linju <linju@douban.com>
2014-04-22 13:44:53 +0800

    Pass test both for python2 & 3.

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -11,13 +11,13 @@ def client():
     return Client(app, BaseResponse)
 
 def test_get_index(client):
-    assert client.get('/').data == 'Index'
+    assert client.get('/').data == b'Index'
 
 def test_get_hello(client):
-    assert client.get('/hello').data == 'Hello World: [Anon]'
+    assert client.get('/hello').data == b'Hello World: [Anon]'
 
 def test_get_hello_with_query(client):
-    assert client.get('/hello?name=ainesmile').data == 'Hello World: ainesmile'
+    assert client.get('/hello?name=ainesmile').data == b'Hello World: ainesmile'
 
 def test_get_404(client):
     assert client.get('/404').status_code == 404
