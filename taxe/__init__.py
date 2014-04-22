50c95e93d996acea868e034f3b6306809c8cc07e
linju <linju@douban.com>
2014-04-22 08:54:44 +0800

    Dispatch url to route.

--- taxe/__init__.py
+++ taxe/__init__.py
@@ -2,23 +2,39 @@
 
 from functools import wraps
 from werkzeug.wrappers import Request, Response
+from werkzeug.exceptions import HTTPException
+from werkzeug.routing import Map, Rule
 
-class Application(object):
+class Taxe(object):
 
-    def route(self, url):
-        def deco(function):
-            @wraps(function)
-            def _(*args, **kwargs):
-                print self, url
-                return function(*args, **kwargs)
-            return _
-        return deco
+    def __init__(self):
+        self.urls = {}
+        self.views = Map()
 
-    @Request.application
-    def __call__(self, request):
-        return Response('Hello World')
+    def build(self, urls):
+        self.urls = urls
+        for key in urls:
+            rule = Rule(key, methods=('GET', ), endpoint=key)
+            self.views.add(rule)
+
+    def dispatch_request(self, request):
+        adapter = self.views.bind_to_environ(request.environ)
+        try:
+            endpoint, values = adapter.match()
+            response = self.urls[endpoint]()
+            return Response(response)
+        except HTTPException, e:
+            return e
+
+    def wsgi_app(self, environ, start_response):
+        request = Request(environ)
+        response = self.dispatch_request(request)
+        return response(environ, start_response)
+
+    def __call__(self, environ, start_response):
+        return self.wsgi_app(environ, start_response)
 
 if __name__ == '__main__':
     from werkzeug.serving import run_simple
-    application = Application()
+    application = Taxe()
     run_simple('localhost', 4000, application)
