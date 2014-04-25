20e4681f911055b15994c77417de267478da7ba7
soasme <soasme@gmail.com>
2014-04-25 16:11:12 +0800

    Unittest for basic example.

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -2,6 +2,8 @@
 
 from .suites.hello import app
 
+# functional tests
+
 def test_get_index():
     assert app.client.get('/').data == b'Index'
 
@@ -13,3 +15,16 @@ def test_get_hello_with_query():
 
 def test_get_404():
     assert app.client.get('/404').status_code == 404
+
+# unit test
+
+from .suites.hello import index, hello
+
+def test_index():
+    assert index() == 'Index'
+
+def test_hello_for_anon():
+    assert hello({}) == 'Hello World: [Anon]'
+
+def test_hello_for_name():
+    assert hello({'name': 'ainesmile'}) == 'Hello World: ainesmile'
