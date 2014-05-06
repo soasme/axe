d737feabf56d6c595957114a665ee9704df9ac64
soasme <soasme@gmail.com>
2014-05-06 08:51:32 +0800

    Test proxy, add_static_folder, add_route_dispatcher.

--- tests/test_axe.py
+++ tests/test_axe.py
@@ -7,6 +7,10 @@ from axe import Axe, errors
 def axe():
     return Axe()
 
+@pytest.fixture
+def axer():
+    return Axe()
+
 def test_build_from_urls(axe):
     func = lambda: ''
     axe.build({'/': func})
@@ -28,3 +32,21 @@ def test_register_ext_duplicated(axe):
 def test_unrecognized_ext(axe):
     with pytest.raises(errors.UnrecognizedExtension):
         axe.build({'/': lambda unrecognized_ext: ''})
+
+def test_proxy(axe, axer):
+    axer.build({'/': lambda: 'hello', '/hello': lambda: 'world'})
+    axe.proxy({'/axer': axer})
+    assert axe.client.get('/axer').data == b'hello'
+    assert axe.client.get('/axer/hello').data == b'world'
+
+def test_add_route_dispatcher(axe, axer):
+    axer.build({'/': lambda: 'hello', '/hello': lambda: 'world'})
+    axe.add_route_dispatcher('/axer', axer)
+    assert axe.client.get('/axer').data == b'hello'
+    assert axe.client.get('/axer/hello').data == b'world'
+
+def test_add_static_folder_success(axe, tmpdir):
+    p = tmpdir.mkdir("sub").join("hello.css")
+    p.write("body { background: black; }")
+    axe.add_static_folder('/media', tmpdir.strpath)
+    assert axe.client.get('/media/sub/hello.css').data == p.read()
