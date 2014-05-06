a8ce2fd8e20cfae0e5d5114ff16f268f4b9c6f93
soasme <soasme@gmail.com>
2014-05-06 09:17:26 +0800

    Unittest for axe get_view_args.

--- tests/test_axe.py
+++ tests/test_axe.py
@@ -33,6 +33,44 @@ def test_unrecognized_ext(axe):
     with pytest.raises(errors.UnrecognizedExtension):
         axe.build({'/': lambda unrecognized_ext: ''})
 
+def test_get_ext_by_name(axe):
+    @axe.ext
+    def test(request):
+        pass
+    assert axe.get_ext('test') == test
+
+def test_get_view(axe):
+    view = lambda: 'view'
+    axe.build({'/view': view})
+    assert axe.get_view('/view') == view
+
+def test_if_view_does_not_exist(axe):
+    pytest.raises(errors.MissingEndpoint, axe.get_view, '/view')
+
+def test_get_request_ext_if_view_has_only_one_request(axe):
+    view = lambda request: ''
+    axe.build({'/': view})
+    assert axe.get_view_args(view, 'request') == {'request': 'request'}
+
+def test_get_request_ext_with_other(axe):
+    @axe.ext
+    def others(request):
+        return 'others'
+    view = lambda request, others: ''
+    axe.build({'/': view})
+    assert axe.get_view_args(view, 'request') == {
+        'request': 'request',
+        'others': 'others'
+    }
+
+def test_get_ext_without_request(axe):
+    @axe.ext
+    def whatever(request):
+        return 'whatever'
+    view = lambda whatever: ''
+    axe.build({'/': view})
+    assert axe.get_view_args(view, 'request') == {'whatever': 'whatever'}
+
 def test_proxy(axe, axer):
     axer.build({'/': lambda: 'hello', '/hello': lambda: 'world'})
     axe.proxy({'/axer': axer})
@@ -47,6 +85,8 @@ def test_add_route_dispatcher(axe, axer):
 
 def test_add_static_folder_success(axe, tmpdir):
     p = tmpdir.mkdir("sub").join("hello.css")
-    p.write("body { background: black; }")
+    content = b"body { background: black; }"
+    p.write(content)
     axe.add_static_folder('/media', tmpdir.strpath)
-    assert axe.client.get('/media/sub/hello.css').data == p.read()
+    assert axe.client.get('/media/sub/hello.css').data == content
+
