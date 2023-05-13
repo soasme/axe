3e43d69ebca679e5d22c76805c44689107adeb26
soasme <soasme@gmail.com>
2023-05-13 14:23:32 +1200

    upgrade version.

--- setup.py
+++ setup.py
@@ -1,37 +1,14 @@
 # -*- coding: utf-8 -*-
 """
-- What is Axe?
+# Axe
 
-Axe is an extremely extendable web framework for Python based on `Werkzeug`.
-It help developer keep project easy to extend and test when project grows.
-
-Unlike Flask, there is no **Thread-Local** variables like `flask.request`, `flask.g`.
-All variable are injected into view function through function name inspired by `py.test fixture`.
-
-**Warning**: It's still experimental and has many buggy.
-
-- Example
-
-    from axe import Axe
-    import os
-    app = Axe()
-
-    @app.ext
-    def config():
-        return {'system': os.name}
-
-    def index(config):
-        return config.get('system', 'Unknown')
-
-    app.build({'/': index})
-
-    if __name__ == '__main__':
-        app.run_simple()
+Axe is a Python Generative AI toolkit.
+It offers a set of tools for building AI framework easier.
 
 
-- Where can I get help?
+## Where can I get help?
 
-You can ask any question in [Github Issue](https://github.com/soasme/axe/issues)  :)
+Please report issues on [Github Issue](https://github.com/soasme/axe/issues).
 Read documentation here: http://axe.rtfd.org
 """
 
@@ -39,13 +16,13 @@ from setuptools import setup
 
 setup(
     name="Axe",
-    version='0.0.4',
+    version='0.0.5',
     author="Ju Lin",
     author_email="soasme@gmail.com",
-    description="An Extendable Python Web Framework",
+    description="Axe is a Python Generative AI toolkit",
     long_description=__doc__,
     license="MIT License",
-    keywords="Web frameword",
+    keywords="AI,GPT,GenerativeAI,Transformer",
     url="https://github.com/soasme/axe",
     packages=['axe'],
     classifiers=[
